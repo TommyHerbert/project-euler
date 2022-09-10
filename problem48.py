c5c96c5839b1ceb2a9157b719a66b2455437c3b5
Tommy Herbert <tommyherbert@gmail.com>
2022-09-10 16:19:46 +0100

    Optimise solution for problem 48

--- problem48.py
+++ problem48.py
@@ -10,6 +10,12 @@ def add_power(running_total, base, exponent, modulus):
 def solve(maximum, modulus):
     running_total = 0
     for i in range(1, maximum + 1):
-        running_total = add_power(running_total, i, i, modulus)
+        running_total = add_power_2(running_total, i, i, modulus)
     return running_total
 
+
+def add_power_2(running_total, base, exponent, modulus):
+    working_product = 1
+    for i in range(exponent):
+        working_product = (working_product * base) % modulus
+    return (running_total + working_product) % modulus
