73588b984b0fd419dc63af6d768822f0511f681e
Tommy Herbert <tommyherbert@gmail.com>
2022-09-10 15:11:42 +0100

    Complete solution

--- problem47.py
+++ problem47.py
@@ -1,8 +1,27 @@
+from math import sqrt
+
 primes = [2]
 
+
 def factorise(n):
-    
+    root = sqrt(n)
     for prime in primes:
+        if prime > root:
+            primes.append(n)
+            return set([n])
         if n % prime == 0:
-            return [prime] + factorise(n / prime)
+            return set([prime]).union(factorise(int(n / prime)))
+    primes.append(n)
+    return set([n])
+
+
+def solve(n):
+    pointer = 2
+    while True:
+        count = 0
+        while len(factorise(pointer + count)) == n:
+            count += 1
+        if count == n:
+            return pointer
+        pointer += 1
 
